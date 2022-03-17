4b3f2a340cc7ef6cf7d3c4bf75b73049eaf8af0b
CrlsDev <c6099020@gmail.com>
2022-03-16 19:02:49 -0500

    Arreglo de includes

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <fstream>
-#include ".\modulos\funciones.cpp" //se incluyo el archivo de funciones
+#include ".\modulos\funciones.hpp" //se incluyo el archivo de funciones
 
 using namespace std;
 int main(int argc, char** argv) {
