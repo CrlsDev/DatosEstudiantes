41188af3ade34aa70a82df2df63f02ff0f101768
CrlsDev <c6099020@gmail.com>
2022-03-16 20:42:58 -0500

    Control de datos actualizado

--- modulos/ControlDatos.hpp
+++ modulos/ControlDatos.hpp
@@ -3,12 +3,13 @@
 #include <string>
 #include ".\funciones.hpp"
 #include ".\Nodo.hpp"
+#define SEPARADOR ";"
 bool Guardar(Nodo* cab,const char* nombre){
   std::ofstream archivo(".\\Datos\\"+std::string(nombre));
   if (!archivo) return false;
 
   while (cab!=NULL){
-    archivo<<cab->id<<";"<<cab->nota<<std::endl;
+    archivo<<cab->id<<SEPARADOR<<cab->nota<<std::endl;
     cab = cab->sig;
   }
   archivo.close();
@@ -17,4 +18,13 @@ bool Guardar(Nodo* cab,const char* nombre){
 Nodo* Cargar(const char* nombre){
   std::ifstream archivo(".\\Datos\\"+std::string(nombre));
   if (!archivo) return NULL;
+  Nodo* cab = NULL;
+  std::string linea;
+  long id;
+  float nota;
+  while(!archivo.eof()){
+    std::getline(archivo,linea);
+    
+    colaMeter(cab,id,nota);
+  }
 }
