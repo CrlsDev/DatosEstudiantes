695e3dabb8f17bf9fae9a07f7dd59eee866e3afd
johangxt <sebasj551@gmail.com>
2022-03-17 19:25:17 -0500

    descarga del proyecto

--- main.cpp
+++ main.cpp
@@ -4,5 +4,13 @@
 
 using namespace std;
 int main(int argc, char** argv) {
+  Nodo* cabeza=NULL;
 
+  long id=1212;
+  float nota=1.5;
+  //cout<<"meter id:"<<id;
+ // cout<<"meter nota;"<<nota;
+  colaMeter(cabeza, id, nota);
+  colaMeter(cabeza, 12 , 1213);
+  Mostrar(cabeza);
 }
