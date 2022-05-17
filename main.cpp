9a6873b012874a9308cc0e8d88ee85b27641eb77
CarlosC <Carlos.manuelct17@gmail.com>
2022-05-16 21:02:17 -0500

    modularizacion

--- main.cpp
+++ main.cpp
@@ -17,130 +17,16 @@ int main(int argc, char** argv) {
     switch (op1)
     {
       case 1: //SELECCIONAR OPCION PILA O COLA.
-        
-        do{
-          system ("CLS");
-          PosicionarCursor(45,10); cout<<"ESCOJA UNA OPCION\n";
-          PosicionarCursor(45,12); cout<<"1.............Pila\n";
-          PosicionarCursor(45,13); cout<<"2.............Cola\n";
-          PosicionarCursor(45,14); cout<<"opcion:";
-          PosicionarCursor(52,14);cin>>modo;
-        }while(modo!=1 && modo!=2);
-        if (modo==1){
-          texto_modo = "PILA";
-          puntero=&pila;
-        }else if (modo==2){
-          texto_modo = "COLA";
-          puntero=&cola;
-        }
+         Escoger_Modo();
         break;
       case 2: //METER DATO
         
-        if (puntero==NULL)
-        {
-          cout<<"Elija un modo."<<endl;
-          system("pause");
-          break;
-        }
-        {
-          //long id; float nota; const char* nombre;
-          Estudiante* e=new Estudiante;
-          bool valido, copiado=false;
-        do{
-          valido = true;
-        system ("CLS");
-        PosicionarCursor(44,11); cout<<"INGRESANDO DATO EN: "<<texto_modo;
-        PosicionarCursor(45,15); cout<<"Digite id:";cin>>e->id;
-        if (modo==1)
-        {
-          if (Buscar(pila,e->id)){
-            PosicionarCursor(45,17); cout<<"Esta id ya existe"<<endl;
-            system("pause");
-            valido=false;
-            continue;
-          }
-          Estudiante* coincidencia = BuscarId(cola,e->id);
-          if (coincidencia!=NULL){
-            valido=false;
-            int pregunta;
-            do{
-              cout<<"La id que intente ingresar ya existe en el otro modo, Desea copiar el dato a la pila?\n 1. Si | 2. No\n";
-              cin>>pregunta;
-
-            }while(pregunta!=1 && pregunta!=2);
-            if ((valido=pregunta==1)){pilaMeter(pila, coincidencia);copiado=true;}
-          }
-
-        }else if (modo==2){
-          if (Buscar(cola,e->id)){
-            PosicionarCursor(45,17); cout<<"Esta id ya existe"<<endl;
-            system("pause");
-            valido=false;
-            continue;
-          }
-          Estudiante* coincidencia = BuscarId(pila,e->id);
-          if (coincidencia!=NULL){
-            valido=false;
-            int pregunta;
-            do{
-              cout<<"La id que intente ingresar ya existe en el otro modo, Desea copiar el dato a la cola?\n 1. Si | 2. No\n";
-              cin>>pregunta;
-
-            }while(pregunta!=1 && pregunta!=2);
-            if ((valido=pregunta==1)){colaMeter(cola, coincidencia);copiado=true;}
-          }
-        
-        }
-        
-        } while ((e->id<=0) || !valido);
-        if (!copiado){
-        do{
-        system ("CLS");
-        PosicionarCursor(44,11); cout<<"INGRESANDO DATO EN: "<<texto_modo;
-        PosicionarCursor(45,15); cout<<"Digite id:"<<e->id;
-        PosicionarCursor(45,16); cout<<"Digite nota (de 0 a 5):";cin>>e->nota;
-        } while (!(e->nota<=5 && e->nota>=0));
-        
-      
-        system ("CLS");
-        PosicionarCursor(44,11); cout<<"INGRESANDO DATO EN: "<<texto_modo;
-        PosicionarCursor(45,15); cout<<"Digite id:"<<e->id;
-        PosicionarCursor(45,16); cout<<"Digite nota (de 0 a 5):"<<e->nota;
-        
-        PosicionarCursor(45,17); cout<<"Digite el nombre del estudiante:";cin>>e->nombre;
-
-
-        if (modo==1)
-        {
-        pilaMeter(pila,e);
-        }else if (modo==2)
-        {
-        colaMeter(cola,e);
-        }}
-		  }
+         Meter_dato();
       
         break;
       case 3: // prueba sacar
-      if (puntero==NULL)
-        {
-          cout<<"Elija un modo."<<endl;
-          system("pause");
-          break;
-        }
-       { 
-          Estudiante*datoSacar=sacar(*puntero); 
-          PosicionarCursor(45,18);
-          if (datoSacar!=NULL){
-          cout<<"Saco : ";
-          PosicionarCursor(45,20);
-          mostrarEstudiante(datoSacar);
-          }else
-          {
-            cout<<"la estructura esta vacia."<<endl;
-          }
-          
-          system("pause");
-         }
+      
+       
       break;
 
       case 4: //mostrar
--- modulos/menu.hpp
+++ modulos/menu.hpp
@@ -10,7 +10,7 @@ Nodo**puntero=NULL;
 const char* texto_modo = "(NO SE HA ELEGIDO MODO)"; 
 float nota_minima =3.0;
 
-void Menu_principal(){
+void Menu_principal(){ 
 	system ("CLS");
     PosicionarCursor(45,2); cout<<"MENU PRINCIPAL "<<texto_modo<<"\n";
     PosicionarCursor(40,6); cout<<"1.............Elegir modo\n";
@@ -26,3 +26,130 @@ void Menu_principal(){
     PosicionarCursor(40,17);cout<<"Opcion: \n";
     PosicionarCursor(48,17);cin>>op1;
 }
+
+    void Escoger_Modo(){
+        do{
+          system ("CLS");
+          PosicionarCursor(45,10); cout<<"ESCOJA UNA OPCION\n";
+          PosicionarCursor(45,12); cout<<"1.............Pila\n";
+          PosicionarCursor(45,13); cout<<"2.............Cola\n";
+          PosicionarCursor(45,14); cout<<"opcion:";
+          PosicionarCursor(52,14);cin>>modo;
+        }while(modo!=1 && modo!=2);
+        if (modo==1){
+          texto_modo = "PILA";
+          puntero=&pila;
+        }else if (modo==2){
+          texto_modo = "COLA";
+          puntero=&cola;
+        }
+    }
+
+    void Meter_dato()
+    
+        {
+            if (puntero==NULL)
+        {
+          cout<<"Elija un modo."<<endl;
+          system("pause");
+          return;
+        }
+          //long id; float nota; const char* nombre;
+          Estudiante* e=new Estudiante;
+          bool valido, copiado=false;
+        do{
+          valido = true;
+        system ("CLS");
+        PosicionarCursor(44,11); cout<<"INGRESANDO DATO EN: "<<texto_modo;
+        PosicionarCursor(45,15); cout<<"Digite id:";cin>>e->id;
+        if (modo==1)
+        {
+          if (Buscar(pila,e->id)){
+            PosicionarCursor(45,17); cout<<"Esta id ya existe"<<endl;
+            system("pause");
+            valido=false;
+            continue;
+          }
+          Estudiante* coincidencia = BuscarId(cola,e->id);
+          if (coincidencia!=NULL){
+            valido=false;
+            int pregunta;
+            do{
+              cout<<"La id que intente ingresar ya existe en el otro modo, Desea copiar el dato a la pila?\n 1. Si | 2. No\n";
+              cin>>pregunta;
+
+            }while(pregunta!=1 && pregunta!=2);
+            if ((valido=pregunta==1)){pilaMeter(pila, coincidencia);copiado=true;}
+          }
+
+        }else if (modo==2){
+          if (Buscar(cola,e->id)){
+            PosicionarCursor(45,17); cout<<"Esta id ya existe"<<endl;
+            system("pause");
+            valido=false;
+            continue;
+          }
+          Estudiante* coincidencia = BuscarId(pila,e->id);
+          if (coincidencia!=NULL){
+            valido=false;
+            int pregunta;
+            do{
+              cout<<"La id que intente ingresar ya existe en el otro modo, Desea copiar el dato a la cola?\n 1. Si | 2. No\n";
+              cin>>pregunta;
+
+            }while(pregunta!=1 && pregunta!=2);
+            if ((valido=pregunta==1)){colaMeter(cola, coincidencia);copiado=true;}
+          }
+        
+        }
+        
+        } while ((e->id<=0) || !valido);
+        if (!copiado){
+        do{
+        system ("CLS");
+        PosicionarCursor(44,11); cout<<"INGRESANDO DATO EN: "<<texto_modo;
+        PosicionarCursor(45,15); cout<<"Digite id:"<<e->id;
+        PosicionarCursor(45,16); cout<<"Digite nota (de 0 a 5):";cin>>e->nota;
+        } while (!(e->nota<=5 && e->nota>=0));
+        
+      
+        system ("CLS");
+        PosicionarCursor(44,11); cout<<"INGRESANDO DATO EN: "<<texto_modo;
+        PosicionarCursor(45,15); cout<<"Digite id:"<<e->id;
+        PosicionarCursor(45,16); cout<<"Digite nota (de 0 a 5):"<<e->nota;
+        
+        PosicionarCursor(45,17); cout<<"Digite el nombre del estudiante:";cin>>e->nombre;
+
+
+        if (modo==1)
+        {
+        pilaMeter(pila,e);
+        }else if (modo==2)
+        {
+        colaMeter(cola,e);
+        }}
+		  }
+
+     void prueba_sacar()
+      { 
+         if (puntero==NULL)
+        {
+          cout<<"Elija un modo."<<endl;
+          system("pause");
+          return;
+        }
+          Estudiante*datoSacar=sacar(*puntero); 
+          PosicionarCursor(45,18);
+          if (datoSacar!=NULL){
+          cout<<"Saco : ";
+          PosicionarCursor(45,20);
+          mostrarEstudiante(datoSacar);
+          }else
+          {
+            cout<<"la estructura esta vacia."<<endl;
+          }
+          
+          system("pause");
+         }
+
+    
